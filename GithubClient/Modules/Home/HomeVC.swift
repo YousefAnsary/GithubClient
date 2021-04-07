02096e5fe5de843163b08ed1cc1bd7d319fae817
Yousef <yosefali97@hotmail.com>
2021-04-07 21:07:25 +0200

    Create home table& cell

--- GithubClient/Modules/Home/HomeVC.swift
+++ GithubClient/Modules/Home/HomeVC.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class HomeVC: BaseViewController {
 
+    @IBOutlet private weak var repositoriesTV: UITableView!
+    var presenter: HomePresenter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    private func setupTableView() {
+        repositoriesTV.register(UINib(nibName: "RepositoryCell", bundle: nil), forCellReuseIdentifier: "RepositoryCell")
+        repositoriesTV.delegate = self
+        repositoriesTV.dataSource = self
+        let refreshControl = UIRefreshControl()
+        repositoriesTV.addSubview(refreshControl)
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+    }
+    
+    @objc func refresh() {
+        
+    }
 
 
+}
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryCell") as! RepositoryCell
+        return cell
+    }
+    
 }
 
