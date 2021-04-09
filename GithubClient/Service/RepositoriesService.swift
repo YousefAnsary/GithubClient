0c32d0acdbc53672587e5aa42a14ac9c23b645ad
Yousef <yosefali97@hotmail.com>
2021-04-09 14:17:45 +0200

    Refactor Fake Response Feature

--- GithubClient/Service/RepositoriesService.swift
+++ GithubClient/Service/RepositoriesService.swift
@@ -9,27 +9,28 @@ import Foundation
 
 class RepositoriesService {
     
-    class func getRepositories(completion: @escaping(Result<[Repository], Error>)-> Void) {
-        
-//        var params = ["page": String(page), "per_page": "10"]
-//        if name != nil { params["q"] = name! }
-        
-//        let repos = localResponse()
-//        completion(repos != nil ? .success(repos!) : .failure(APIError.other(statusCode: 0, data: nil, error: nil)))
-//        return
-        
-        APIManager.default.get(path: "repositories", parameters: nil) { data, res, err in
+    /// Calls Github API to fetch public repos
+    /// - Parameters:
+    ///   - locally: False by default, if set to true it loads local faked response
+    ///   - completion: Result Closure which is fired on completion of request
+    class func getRepositories(locally: Bool = false, completion: @escaping(Result<[Repository], Error>)-> Void) {
+        if locally {
+            let repos = fetchLocally()
+            completion(repos == nil ? .failure(APIError.other(statusCode: 0, data: nil, error: nil)) : .success(repos!))
+        } else {
+            fetchRemotely(completion: completion)
+        }
+    }
+    
+    private class func fetchRemotely(completion: @escaping(Result<[Repository], Error>)-> Void) {
+        APIManager.default.get(path: "repositories", parameters: nil) { _data, res, err in
             guard err == nil else {
                 completion(.failure(err!))
                 return
             }
             let statusCode = (res as? HTTPURLResponse)?.statusCode ?? 0
-            guard 200...299 ~= statusCode else {
-                completion(.failure(APIError(rawValue: statusCode, data: data)))
-                return
-            }
-            guard let data = data else {
-                completion(.failure(APIError.other(statusCode: statusCode, data: nil, error: err)))
+            guard 200...299 ~= statusCode, let data = _data else {
+                completion(.failure(APIError(rawValue: statusCode, data: _data)))
                 return
             }
             
@@ -39,11 +40,10 @@ class RepositoriesService {
             } catch (let error) {
                 completion(.failure(APIError.decodingFailed(error: error)))
             }
-            
         }
     }
     
-    private class func localResponse()-> [Repository]? {
+    private class func fetchLocally()-> [Repository]? {
         guard let path = Bundle.main.path(forResource: "Response", ofType: "json"),
               let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe),
               let repos = try? JSONHelper.shared.decode(data, to: [Repository].self) else {
